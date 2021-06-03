41a09adb38d2705a8b47fa8f165431bc3f2136d9
manoj anasuri <manojanasuri16@gmail.com>
2021-06-03 23:14:15 +0530

    Revert "Merge branch 'main' of https://github.com/manojanasuri16/Full-Stack-Open into main"
    This reverts commit 94a29ee5e58ddd90874bf6159df48be3850abea4, reversing
    changes made to 597c955529bd4ac37878b2be89206c4eff896345.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,44 +1,50 @@
 import React,{useState} from 'react';
 
-const App = () => {  
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const App = () =>{
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad] = useState(0)
+
+  const goodFeedback = () => setGood(good  + 1)
+  const neutralFeedback  = () => setNeutral(neutral + 1)
+  const badFeedback = () => setBad(bad + 1)
 
   return (
     <div>
       <h2>give feedback</h2>
       <div>
-        <button onClick={()=>setGood(good+1)}>good</button>
-        <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-        <button onClick={()=>setBad(bad+1)}>bad</button>    
-      </div>      
-      <h2>Statistics</h2>
+        <Button text="good" event={goodFeedback}/>
+        <Button text="neutral" event={neutralFeedback}/>
+        <Button text="bad" event={badFeedback}/>
+      </div>
+      <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad}/>
-    </div>  
-  )
+    </div>
+  );
 }
 
-const Statistics = ({good,neutral,bad}) => { 
-  if(good!==0 || bad!==0 || neutral!==0){
-    const calTotal=()=>good+neutral+bad
-    const calAverage=()=>((good*1)+(neutral*0)+(bad*-1))/calTotal()
-    const calPositive=()=>(good/calTotal())*100
-    return(      
-     <table>
-      <tbody>
-        <Statistic text="good" value={good}/>
-        <Statistic text="neutral" value={neutral}/>
-        <Statistic text="bad" value={bad}/>
-        <Statistic text="all" value={calTotal()}/>
-        <Statistic text="average" value={calAverage()}/>
-        <Statistic text="positive" value={calPositive()}/>
-      </tbody>
-      </table>       
-    )    
+const Statistics = ({good,neutral,bad}) => {
+
+  if(good !== 0 || neutral!==0 || bad !== 0){
+    const Total = () => good + bad + neutral
+    const Avg  = () =>  ((1*good) + (0*neutral) + (-1 * bad))/Total()
+    const Positive=()=> (good / Total()) * 100
+
+    return (
+      <table>
+        <tbody>
+          <Statistic text="good" value ={good}/>
+          <Statistic text="neutral" value ={neutral}/>
+          <Statistic text="bad" value ={bad}/>
+          <Statistic text="all" value ={Total()}/>
+          <Statistic text="average" value ={Avg()}/>
+          <Statistic text="positive" value ={Positive()}/>
+        </tbody>
+      </table>
+    )
   }
   else{
-    return(
+    return (
       <div>
         <p>No feedback given</p>
       </div>
@@ -46,13 +52,20 @@ const Statistics = ({good,neutral,bad}) => {
   }
 }
 
-const Statistic=({text,value})=>{
-  return(
+const Statistic = ({text,value}) => {
+  return (
     <tr>
-      <td>{text}</td>      
+      <td>{text}</td>
       <td>{value}</td>
-    </tr>    
+    </tr>
+  )
+}
+const Button = ({text,event}) => {
+  return(
+    <button onClick={event}>
+      {text}
+    </button>
   )
 }
 
-export default App
+export default App;
