389d1525c7fb6266811f702f00a906d98aaa58d1
manoj anasuri <manojanasuri16@gmail.com>
2021-06-09 19:54:05 +0530

    Update App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,71 +1,62 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
-const App = () =>{
-  const [good,setGood] = useState(0)
-  const [neutral,setNeutral] = useState(0)
-  const [bad,setBad] = useState(0)
+const App = () => {  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const goodFeedback = () => setGood(good  + 1)
-  const neutralFeedback  = () => setNeutral(neutral + 1)
-  const badFeedback = () => setBad(bad + 1)
+  const G=()=>setGood(good+1)
+  const N=()=>setNeutral(neutral+1)
+  const B=()=>setBad(bad+1)
 
   return (
     <div>
       <h2>give feedback</h2>
-      <div>
-        <Button text="good" event={goodFeedback}/>
-        <Button text="neutral" event={neutralFeedback}/>
-        <Button text="bad" event={badFeedback}/>
-      </div>
+      <Button event={G} text='good'/>
+      <Button event={N} text='neutral'/>
+      <Button event={B} text='bad'/>
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
-  );
+  )
+}
+
+const Button=({text,event})=>{
+  return(
+    <button onClick={event}>{text}</button>
+  )
+}
+
+const Statistic=(props)=>{
+  return(
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
 }
 
 const Statistics = ({good,neutral,bad}) => {
-
-  if(good !== 0 || neutral!==0 || bad !== 0){
-    const Total = () => good + bad + neutral
-    const Avg  = () =>  ((1*good) + (0*neutral) + (-1 * bad))/Total()
-    const Positive=()=> (good / Total()) * 100
-
-    return (
-      <table>
-        <tbody>
-          <Statistic text="good" value ={good}/>
-          <Statistic text="neutral" value ={neutral}/>
-          <Statistic text="bad" value ={bad}/>
-          <Statistic text="all" value ={Total()}/>
-          <Statistic text="average" value ={Avg()}/>
-          <Statistic text="positive" value ={Positive()}/>
+  const Total=()=>good+neutral+bad
+  const average=()=> (good*1+neutral*0+bad*-1)/Total()
+  const positive=()=>(good/Total())*100
+  if(good===0 && neutral===0 && bad===0){
+    return(<div>No feedback given</div>)    
+  }
+  else{
+    return(
+      <table> 
+        <tbody>       
+          <Statistic text='good' value={good}/>
+          <Statistic text='neutral' value={neutral}/>
+          <Statistic text='bad' value={bad}/>
+          <Statistic text='all' value={Total()}/>
+          <Statistic text='average' value={average()}/>
+          <Statistic text='positive' value={positive()+' %'}/>        
         </tbody>
       </table>
     )
   }
-  else{
-    return (
-      <div>
-        <p>No feedback given</p>
-      </div>
-    )
-  }
 }
 
-const Statistic = ({text,value}) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-const Button = ({text,event}) => {
-  return(
-    <button onClick={event}>
-      {text}
-    </button>
-  )
-}
-
-export default App;
+export default App
